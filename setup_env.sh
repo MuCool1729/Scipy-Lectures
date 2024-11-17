6f54295d38c61cc0f7b925c4bc1c3aa9c4ab54f1
MuCool1729 <Mukul1729@hotmail.com>
2024-11-17 16:11:41 +0000

    setup changes

--- setup_env.sh
+++ setup_env.sh
@@ -5,4 +5,5 @@ rm -rf venv
 python3 -m venv venv/scipy_lectures
 source venv/scipy_lectures/bin/activate
 pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r requirements.txt
